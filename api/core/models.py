2f683a3f32172b1ac6cf396be8e1798440058259
aoddy10 <vobi69p@gmail.com>
2025-02-12 12:49:50 +1300

    docs: add reset password endpoint to API Documentation.

--- api/core/models.py
+++ api/core/models.py
@@ -1,14 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Item(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
 class User(AbstractUser):
     ROLE_CHOICES = [
         ("user", "User"),
