d9995a880f47a0416c9c487fabd22c1012b55410
phyowan <phyomgmgwan@gmail.com>
2025-02-12 10:50:20 +1300

    adjusting the folder structure

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import Home from './home';
+import Home from './pages/homepage/home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
