b217da9841dc7a7f19cdec6aa6fb61ea75373014
aoddy10 <vobi69p@gmail.com>
2025-02-14 13:14:34 +1300

    feat: display review admin on admin site.

--- api/review/admin.py
+++ api/review/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "location", "rating", "created_at")
+    list_filter = ("rating", "location", "user")
+    search_fields = ("user__username", "location__title", "review")
+    ordering = ("-created_at",)
