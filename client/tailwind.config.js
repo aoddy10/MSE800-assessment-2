3acfea7308c698869cec816adec962c5979a8dfb
aoddy10 <vobi69p@gmail.com>
2025-02-18 09:58:03 +1300

    style: setup tailwind theme for font and color.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#31AAB7",
+                secondary: "#767676",
+                accent: "#31AAB7",
+                background: "#F9F9FB",
+                text: "#232323",
+            },
+            fontFamily: {
+                bold: ["LatoBold", "sans-serif"],
+                medium: ["LatoMedium", "sans-serif"],
+                regular: ["LatoRegular", "sans-serif"],
+                semibold: ["LatoSemibold", "sans-serif"],
+                thin: ["LatoThin", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
