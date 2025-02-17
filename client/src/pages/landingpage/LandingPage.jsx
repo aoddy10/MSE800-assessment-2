a0581d333372f8089b011788631d70bc1633b8a4
aoddy10 <vobi69p@gmail.com>
2025-02-18 09:22:44 +1300

    refactor: move landing page and component in to the folder. Update app.jsx to match with the updated code. Change /home to be /explore.

---
+++ client/src/pages/landingpage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CanvasAnimation from "../../components/CanvasAnimation";
+import Navigation from "../../components/Navigation";
+
+function LandingPage() {
+    return (
+        <>
+            <Navigation />
+            <CanvasAnimation />
+        </>
+    );
+}
+
+export default LandingPage;
