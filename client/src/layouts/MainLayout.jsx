553aae94a6e704af9691b44d3a10d4012f7172ae
aoddy10 <vobi69p@gmail.com>
2025-02-18 13:34:56 +1300

    feature: create nav for main layout.

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,18 +1,11 @@
 import { Outlet, Link } from "react-router-dom";
+import NavigationMain from "../components/Navigation-main";
 
 const MainLayout = () => {
     return (
         <div className="min-h-screen flex flex-col">
             {/* Navbar */}
-            <nav className="bg-gray-900 text-white p-4 flex justify-between">
-                <h1 className="text-xl font-bold">MyApp</h1>
-                <div>
-                    <Link to="/home" className="mr-4">
-                        Home
-                    </Link>
-                    <Link to="/login">Login</Link>
-                </div>
-            </nav>
+            <NavigationMain />
 
             {/* Page Content */}
             <div className="flex-grow p-6">
