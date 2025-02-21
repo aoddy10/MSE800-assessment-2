bd27bdb0b8724718b305e01861d9fd28e28365b1
terencelyle21 <lyleborromeo21@gmail.com>
2025-02-22 08:47:00 +1300

    feat: added about, contact, maori, and updated the navbar and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,16 @@ import { AuthProvider } from "./context/AuthContext";
 import LandingPage from "./pages/landingpage/LandingPage";
 //import HomePage from "./pages/HomePage";
 import HomePage from "./pages/homepage/home";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/auth/LoginPage";
 import MainLayout from "./layouts/MainLayout";
 import ProtectedLayout from "./layouts/ProtectedLayout";
 import CityPage from "./pages/CityPage";
 import AdminUserPage from "./pages/admin/AdminUserPage";
 import AdminCityPage from "./pages/admin/AdminCityPage";
 import AdminLocationPage from "./pages/admin/AdminLocationPage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import MaoriPage from "./pages/MaoriPage";
 
 
 
@@ -26,6 +29,9 @@ function App() {
                     <Route element={<MainLayout />}>
                         <Route path="/city/:id" element={<CityPage />} />
                         <Route path="/explore" element={<HomePage />} />
+                        <Route path="/contact" element={<ContactPage />} />
+                        <Route path="/about" element={<AboutPage />} />
+                        <Route path="/maori" element={<MaoriPage />} />
                     </Route>
 
                     {/* Protected Layout */}
