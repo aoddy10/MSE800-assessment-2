250334456968403b4f96626fb944bba4977ee91d
aoddy10 <vobi69p@gmail.com>
2025-02-23 10:19:14 +1300

    feat: verify user role for admin to create and update.

--- api/city/views.py
+++ api/city/views.py
@@ -6,6 +6,9 @@ from rest_framework.decorators import api_view
 from .models import City
 from .serializers import CitySerializer
 
+def is_admin(user):
+    return user.is_authenticated and user.role in ["admin", "business"]
+
 # Get all cities
 @api_view(["GET"])
 def get_cities(request):
@@ -27,6 +30,9 @@ def get_city(request, city_id):
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def create_city(request):
+    if not is_admin(request.user):
+        return Response({"error": "Permission denied"}, status=403)
+    
     serializer = CitySerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -37,6 +43,10 @@ def create_city(request):
 @api_view(["PUT"])
 @permission_classes([IsAuthenticated])
 def update_city(request, city_id):
+    
+    if not is_admin(request.user):
+        return Response({"error": "Permission denied"}, status=403)
+    
     try:
         city = City.objects.get(id=city_id)
     except City.DoesNotExist:
